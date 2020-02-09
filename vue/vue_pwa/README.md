5f275be65d8cfce120e6de90d1a6e54308afa7fb
haolushan5253 <2587051793@qq.com>
2020-02-09 20:44:08 +0800

    add : pwa

---
+++ vue/vue_pwa/README.md
@@ -0,0 +1,16 @@
+# 02_vue_pwa
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
---
+++ vue/vue_pwa/dev/service-worker.js
@@ -0,0 +1,26 @@
+
+
+workbox.core.setCacheNameDetails({prefix: "02_vue_pwa"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+
+// 动态ajax请求的缓存
+workbox.routing.registerRoute(function(obj){
+    // console.log(obj);  // {url: URL, event: FetchEvent}
+    // console.log(obj.url);
+    // 返回是否缓存结果
+    if (obj.url.pathname.includes('getlunbo')) {
+        return true;
+    } else {
+        return false;
+    }
+    // 缓存策略
+},workbox.strategies.cacheFirst());
